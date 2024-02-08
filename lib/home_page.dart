ce018154127cc3cd6ad6b3b154b8a3028eb81df0
Sebastian Miller <sebastian@ux-xu.com>
2024-02-08 13:29:54 +0900

    Use go_router and riverpod

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("テントウムシ"),
+      ),
+      body: const Center(
+        child: Text("Home")
+      ),
+    );
+  }
+}
